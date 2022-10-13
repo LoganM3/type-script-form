2aac27e8f7982ccee12ea13aaf324cf2f6c2d32d
Logan McCalley <mccalleylogan@gmail.com>
2022-10-13 11:54:39 -0400

    skillshare typescript form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+    interface UserInt {
+      name: string,
+      age: string,
+      job: string,
+    }
+  
+    const [userState, setUserState] = useState<{currentUser: UserInt }>({
+      currentUser: {
+        name: "",
+        age: "",
+        job: ""
+      }
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+    <h1>React with Typescript</h1>
+    <form>
+      <label htmlFor="userName">Name:</label>
+      <input 
+        id="userName"
+        type="text"
+        name="name"
+        value=""
+      />
+      <label htmlFor="userAge">Age:</label>
+      <input 
+        id="userAge"
+        type="number"
+        name="age"
+        value=""
+        onChange={onChangeHandler}
+      /> 
+      <label htmlFor="userJob">Job:</label>
+      <input 
+        id="userJob"
+        type="text"
+        name="job"
+        value=""
+      />
+
+      <button type='submit'>Add user</button>
+    </form>
+   </div>
   );
 }
 
